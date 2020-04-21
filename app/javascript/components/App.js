3d877d2f0c6dcdcb752c4628cf1c5d0c36bce528
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2020-04-22 03:50:02 +0500

    Static files path changed

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,10 @@
-import "assets/styles/bootstrap.min.css";
-import "assets/styles/fontawesome.min.css";
-import "assets/styles/animate.min.css";
-import "assets/styles/slick.css";
-import "assets/styles/slick-theme.css";
-import "assets/styles/style.css";
-import "assets/styles/responsive.css";
+import "../assets/styles/bootstrap.min.css";
+import "../assets/styles/fontawesome.min.css";
+import "../assets/styles/animate.min.css";
+import "../assets/styles/slick.css";
+import "../assets/styles/slick-theme.css";
+import "../assets/styles/style.css";
+import "../assets/styles/responsive.css";
 
 import "react-toastify/dist/ReactToastify.css";
 import "react-accessible-accordion/dist/fancy-example.css";
